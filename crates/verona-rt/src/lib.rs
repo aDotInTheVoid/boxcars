38da0b5ebf1448dadd45d4cc002ae656d684143f
Alona Enraght-Moony <code@alona.page>
2023-11-14 20:06:18 +0000

    Note that leaking threads will make us shit the bed.
    Closes #3

--- crates/verona-rt/src/lib.rs
+++ crates/verona-rt/src/lib.rs
@@ -1,3 +1,10 @@
+//!
+//! ## Restictons:
+//!
+//! 1. *Don't leak threads*: When the main thread finishes, all other threads
+//!    shut down. If you've accessed verona-rt resources in other threads,
+//!    you'll have a bad time.
+
 pub mod cown;
 pub mod scheduler;
 pub mod when;
